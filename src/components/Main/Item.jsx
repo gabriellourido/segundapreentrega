53a59d761f6b1a83e72cf0b0c01173a8e866f3df
gabriellourido <112268061+gabriellourido@users.noreply.github.com>
2023-01-12 01:02:29 -0300

    detalles2

--- src/components/Main/Item.jsx
+++ src/components/Main/Item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Item({producto}) {
   return (
     <div className='item'>
-      <img src={producto.img} alt={producto.titulo}/>
+      <img src={producto.img} alt={producto.titulo} className="imgCard"/>
       <div className='cardData'>
         <h2>{producto.titulo}</h2>
         <span className='cardPrecio'>${producto.precio}</span>
